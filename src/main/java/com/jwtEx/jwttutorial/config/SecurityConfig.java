32453641db76c75ccdd1dc80105ef616db519aef
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-10-25 22:13:16 +0900

    Initial commit

---
+++ src/main/java/com/jwtEx/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.jwtEx.jwttutorial.config;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(WebSecurity web ){
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico"
+                );
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
